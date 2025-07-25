091fc58b362eb55fe376ba15275816f3370c8a31
jfracassi84 <joao.fracassi@outlook.com>
2025-07-24 23:14:17 -0300

    Upload
    Atualização do projeto 2.

--- projeto2/GamePokemon.py
+++ projeto2/GamePokemon.py
@@ -1,2 +1,49 @@
-#!/usr/bin/python3
+class Pokemon:
+
+    def __init__(self, especie, level=1, nome=None):
+        self.especie = especie
+        self.level = level
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
+
+    def __str__(self):
+        return "{} ({})".format(self.nome, self.level)
+
+    def atacar(self, pokemon):
+        print(f"{self} atacou {pokemon}")
+
+
+class PokemonEletrico(Pokemon):
+    tipo = "eletrico"
+
+    def atacar(self, pokemon):
+        print(f'{self} lançou um choque do trovão em {pokemon}')
+
+
+class PokemonFogo(Pokemon):
+    tipo = "Fogo"
+
+    def atacar(self, pokemon):
+        print(f'{self} lançou uma bola de fogo na cabeça de {pokemon}')
+
+
+class PokemonAgua(Pokemon):
+    tipo = "Agua"
+
+    def atacar(self, pokemon):
+        print(f'{self} lançou um jato de água em {pokemon}')
+
+
+class Pikachu(PokemonEletrico):
+    especie = "Pikachu"
+
+
+meu_pokemon = PokemonFogo("Charmander")
+pokemon_amigo = PokemonEletrico("Pikachu")
+
+meu_pokemon.atacar(pokemon_amigo)
+pokemon_amigo.atacar(meu_pokemon)
 
