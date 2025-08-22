0317dd1239b580239c7a332041af2e5c7c297f94
crash00xd <joaofracassi@protonmail.com>
2025-08-21 23:00:35 -0300

    Update
    Atualização em todos os projetos.

---
+++ projeto1/files/agenda.py
@@ -0,0 +1,80 @@
+AGENDA = {}
+
+AGENDA['guilherme'] = {
+    'telefone': '999928272',
+    'email': 'guilherme@solyd.com.br',
+    'endereco': 'Av. 1',
+}
+
+AGENDA['maria'] = {
+    'telefone': '999282727',
+    'email': 'maria@solyd.com.br',
+    'endereco': 'Av. 2',
+}
+
+
+def mostrar_contatos():
+    for contato in AGENDA:
+        buscar_contato(contato)
+
+
+def buscar_contato(contato):
+    print('Nome:', contato)
+    print('Telefone:', AGENDA[contato]['telefone'])
+    print('Email:', AGENDA[contato]['email'])
+    print('Endereço:', AGENDA[contato]['endereco'])
+    print('--------------------------------------------')
+
+
+def incluir_editar_contato(contato, telefone, email, endereco):
+    AGENDA[contato] = {
+        'telefone': telefone,
+        'email': email,
+        'endereco': endereco,
+    }
+    print()
+    print('>>>> Contato {} adicionado/editado com sucesso'.format(contato))
+    print()
+
+
+def excluir_contato(contato):
+    AGENDA.pop(contato)
+    print()
+    print('>>>> Contato {} excluido com sucesso'.format(contato))
+    print()
+
+
+def imprimir_menu():
+    print('------------------------------------------')
+    print('1 - Mostrar todos os contatos da agenda')
+    print('2 - Buscar contato')
+    print('3 - Incluir contato')
+    print('4 - Editar contato')
+    print('5 - Excluir contato')
+    print('0 - Fechar agenda')
+    print('------------------------------------------')
+
+
+while True:
+    imprimir_menu()
+
+    opcao = input('Escolha uma opção: ')
+    if opcao == '1':
+        mostrar_contatos()
+    elif opcao == '2':
+        contato = input('Digite o nome do contato: ')
+        buscar_contato(contato)
+    elif opcao == '3' or opcao == '4':
+        contato = input('Digite o nome do contato: ')
+        telefone = input('Digite o nome do telefone: ')
+        email = input('Digite o nome do email: ')
+        endereco = input('Digite o nome do endereco: ')
+        incluir_editar_contato(contato, telefone, email, endereco)
+    elif opcao == '5':
+        contato = input('Digite o nome do contato: ')
+        excluir_contato(contato)
+    elif opcao == '0':
+        print('>>>> Fechando programa')
+        break
+    else:
+        print('>>>> Opção inválida')
--- projeto2/main.py
+++ projeto2/main.py
@@ -1,22 +1,23 @@
-from GamePokemon import *
-from Pessoas import *
+import pickle
+
+from pokemon import *
+from pessoa import *
 
 
 def escolher_pokemon_inicial(player):
-
-    print(f"Olá {player}, você poderá escolher agora o pokemon que irá lhe acompanhar nessa jornada!")
+    print("Olá {}, você poderá escolher agora o Pokemon que irá lhe acompanhar nessa jornada!".format(player))
 
     pikachu = PokemonEletrico("Pikachu", level=1)
     charmander = PokemonFogo("Charmander", level=1)
-    Squirtle = PokemonAgua("Squirtle", level=1)
+    squirtle = PokemonAgua("Squirtle", level=1)
 
-    print("Você possui 3 escolhas:")
+    print("Você possui 3 escolhas: ")
     print("1 -", pikachu)
     print("2 -", charmander)
-    print("3 -", Squirtle)
+    print("3 -", squirtle)
 
-    while(True):
-        escolha = input("Escolha o seu pokemon: ")
+    while True:
+        escolha = input("Escolha o seu Pokemon: ")
 
         if escolha == "1":
             player.capturar(pikachu)
@@ -25,13 +26,79 @@ def escolher_pokemon_inicial(player):
             player.capturar(charmander)
             break
         elif escolha == "3":
-            player.capturar(Squirtle)
+            player.capturar(squirtle)
             break
         else:
-            print("Escolha inválida!")
+            print("Escolha inválida")
 
-player = Player("Guilherme")
-#print(player)
-#player.mostrar_pokemons()
-escolher_pokemon_inicial(player)
-player.mostrar_pokemons()
+
+def salvar_jogo(player):
+    try:
+        with open("database.db", "wb") as arquivo:
+            pickle.dump(player, arquivo)
+            print("Jogo salvo com sucesso!")
+    except Exception as error:
+        print("Erro ao salvar jogo")
+        print(error)
+
+
+def carregar_jogo():
+    try:
+        with open("database.db", "rb") as arquivo:
+            player = pickle.load(arquivo)
+            print("Loading feito com suscesso")
+            return player
+    except Exception as error:
+        print("Save não encontrado")
+
+
+if __name__ == "__main__":
+    print('-----------------------------------------')
+    print("Bem-vindo ao game Pokemon RPG de terminal")
+    print('-----------------------------------------')
+
+    player = carregar_jogo()
+
+    if not player:
+        nome = input("Olá, qual é o seu nome: ")
+        player = Player(nome)
+        print("Olá {}, esse é um mundo habitado por pokemons,"
+              "a partir de agora sua missão é se tornar um mestre dos pokemons!".format(player))
+        print("Capture o máximo de pokemons que conseguir e lute com seus inimigos")
+        player.mostrar_dinheiro()
+
+        if player.pokemons:
+            print("Já ví que você tem alguns pokemons")
+            player.mostrar_pokemons()
+        else:
+            print("Você não tem nenhum pokemon, portanto precisa escolher um")
+            escolher_pokemon_inicial(player)
+
+        print("Pronto, agora que você já possui um pokemon, enfrente seu arqui-rival desde o jardim da infância Gary")
+        gary = Inimigo(nome="Gary", pokemons=[PokemonAgua("Squirtle", level=1)])
+        player.batalhar(gary)
+        salvar_jogo(player)
+
+    while True:
+        print("--------------------------------------")
+        print("O que deseja fazer?")
+        print("1 - Explorar pelo mundão a fora")
+        print("2 - Lutar com um inimigo")
+        print("3 - Ver Pokeagenda")
+        print("0 - Sair do jogo")
+        escolha = input("Sua escolha: ")
+
+        if escolha == "0":
+            print("Fechando o jogo...")
+            break
+        elif escolha == "1":
+            player.explorar()
+            salvar_jogo(player)
+        elif escolha == "2":
+            inimigo_aleatorio = Inimigo()
+            player.batalhar(inimigo_aleatorio)
+            salvar_jogo(player)
+        elif escolha == "3":
+            player.mostrar_pokemons()
+        else:
+            print("Escolha inválida")
